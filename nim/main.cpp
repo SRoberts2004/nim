5874d7f28e03a96ed45994d437ef169c07c27390
Nathan Collier <ncollier1@harding.edu>
2025-04-18 12:42:50 -0500

    outline for both groups with area for the dictionary of variable/function names.  testd

--- nim/main.cpp
+++ nim/main.cpp
@@ -1,1 +1,56 @@
-//Hi.
+
+
+#include <iostream>
+#include <cstring>
+
+
+
+using std::cin;
+using std::cout;
+
+
+
+/*
+1. Let person host or join
+1a. if they select host create the board
+1b. Get name of client and host
+1c. connect both and ensure proper connection
+
+2. board is displayed 
+3. gameplay persues with exchanging of moves
+4. Option Continue without chat, Forfit, Continue withsent chat
+5. send and ensure all relevant information is recieved
+6. check the numbers sent between each 
+7. Display end-game message
+8. send back to step one
+
+
+
+Networking side
+1. connecting servers and letting user pick which role they want
+2. Get names of the players
+3. prompt host to build board
+4. prompt client with first move
+5. exchange moves with or without chat feature
+6. update the board status in between every move
+6a. check for empty board/winner
+7. check for incorrect/illegal moves
+8.validate data before sending
+
+
+
+Client side 
+1. Generate the board
+2. Recieve the board
+3. update the board state
+4. win loss check -
+4a. win
+4b. loss
+4c. forfit
+5. 
+6. 
+
+Common variables and function names:
+1.
+
+*/
