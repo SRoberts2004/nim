3cc61f6964eaa6e6f6b3a354aea7c47c88c56e42
Nathan Collier <ncollier1@harding.edu>
2025-04-23 12:55:15 -0500

    Starting making function "buildDatagram"

--- nim/game.cpp
+++ nim/game.cpp
@@ -7,13 +7,14 @@ using std::cin;
 using std::cout;
 using namespace std;
 
-void displayBoard(vector<string> board) {
+void displayBoard(vector<int> board) {
 	for (int i = 0; i < board.size(); i++) {
 		cout << "Pile #" << i + 1 << " contains " << board.at(i) << " stones." << endl;
 	}
+
 }
 
-vector<string> generateBoard() {
+vector<int> generateBoard() {
 	int numOfPiles;
 	cout << "Enter the number of piles you want here: ";
 	cin >> numOfPiles;
@@ -29,23 +30,43 @@ vector<string> generateBoard() {
 			cout << "Enter a valid number of piles here: ";
 			cin >> numOfPiles;
 		}
+
 	}
 	cout << endl;
-
-	vector<string> board(numOfPiles);
+	//fill in piles with stones
+	vector<int> board(numOfPiles);
 	for (int i = 0; i < numOfPiles; i++) {
 		cout << "Enter the number of stones you'd like in pile #" << i + 1 << " here: ";
 		cin >> board.at(i);
+
+		while (board.at(i) > 20 || board.at(i) < 1) {
+			if (board.at(i) < 1) {
+				cout << "You can not have less than 1 stone in a pile. " << '\n'
+					<< "Please enter in a number that is more than 1" << endl;
+				cin >> board.at(i);
+			}
+			else {
+				cout << "You can not have more than 20 stones in a pile. " << '\n'
+					<< "Please enter in a number that is less than 20" << endl;
+				cin >> board.at(i);
+			}
+		}
 		cout << endl;
 	}
 
-	//We need to add bounds checking to ensure that the user is not allowed to add more than 20 stones per pile.
+
 
 	displayBoard(board);
 	return  board;
 }
+string buildDatagram(vector<int> board) {
+	char cstring[] = "";
+	cstring[0] = board.size();
 
-//int main(){
+
+	return cstring;
+}
+//int main() {
 //
 //	generateBoard();
 //
