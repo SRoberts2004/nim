8eef7fc1425e84c3beee700727877852fe90b5a8
Seth Roberts <sar242004@gmail.com>
2025-04-28 20:51:46 -0500

    Hi

--- nim/main.cpp
+++ nim/main.cpp
@@ -477,12 +477,11 @@ int client_main() {
 						while (nextDecisionDatagram[0] == 'C') {
 							cout << "Enemy has sent a chat message: " << nextDecisionDatagram + 1 << endl;
 							while (true) {
-								recvfrom(ConnectionlessSocket, recvBuf, DEFAULT_BUFLEN, 0, (sockaddr*)&addr, &addrSize);
-								if (strcmp(recvBuf, Study_QUERY) != 0) {
+								recvfrom(ConnectionlessSocket, nextDecisionDatagram, DEFAULT_BUFLEN, 0, (sockaddr*)&addr, &addrSize);
+								if (strcmp(nextDecisionDatagram, Study_QUERY) != 0) {
 									break;
 								}
 							}
-							nextDecisionDatagram = recvBuf;
 						}
 					}
 
